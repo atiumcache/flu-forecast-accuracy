7a3dfbc776a72e863b7391052468f8d35bed65ad
atiumcache <aattilio@pm.me>
2024-07-30 08:29:13 -0700

    get AZ weather api data for trend forecasting.

--- utilities/weather_api.py
+++ utilities/weather_api.py
@@ -27,7 +27,7 @@ def get_single_day_mean_temp(lat: float, long: float, date: str) -> float:
         date: YYYY-MM-DD
 
     Returns:
-        mean temperature (celsius) for given date and location
+        Float. Mean temperature (celsius) for given date and location
     """
     # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
@@ -48,15 +48,85 @@ def get_single_day_mean_temp(lat: float, long: float, date: str) -> float:
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
     # Process daily data. The order of variables needs to be the same as requested.
     daily = response.Daily()
     daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
     return daily_temperature_2m_mean[0]
+
+
+def get_single_day_precip_hours(lat: float, long: float, date: str) -> float:
+    """Returns a precipitation hours for a given location and date.
+
+    Args:
+        lat: latitude for the location
+        long: longitude for the location
+        date: YYYY-MM-DD
+
+    Returns:
+        Float. Precipitation hours for given date and location.
+    """
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": lat,
+        "longitude": long,
+        "start_date": date,
+        "end_date": date,
+        "daily": "precipitation_hours"
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_precipitation_hours = daily.Variables(0).ValuesAsNumpy()
+    return daily_precipitation_hours[0]
+
+
+def get_max_rel_humidity(lat: float, long: float, date: str) -> float:
+    """Returns the maximum relative humidity for a given location and date.
+
+    Args:
+        lat: latitude for the location
+        long: longitude for the location
+        date: YYYY-MM-DD
+
+    Returns:
+        Float. Maximum relative humidity for given date and location.
+    """
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": lat,
+        "longitude": long,
+        "start_date": date,
+        "end_date": date,
+        "hourly": "relative_humidity_2m"
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_relative_humidity_2m = hourly.Variables(0).ValuesAsNumpy()
+    return max(hourly_relative_humidity_2m)
 
 
 def get_weekly_forecast_avg_temp(lat: float, long: float, start_date: str) -> float:
